388db6aac8a7aaa064be91c7b5a71b25da5e549b
Fabio <fabio@chiarani.it>
2022-05-11 09:02:09 +0200

    add HospitalDao

---
+++ droid/app/src/main/java/it/chiarani/prontosoccorsotrentino/data/local/HospitalDao.kt
@@ -0,0 +1,30 @@
+package it.chiarani.prontosoccorsotrentino.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+/**
+ * Represent the functions that interacts with room db
+ */
+@Dao
+interface HospitalDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertHospitalListings(
+        hospitalEntities: List<HospitalEntity>
+    )
+
+    @Query("DELETE FROM hospitalentity")
+    suspend fun clearHospitalListings()
+
+    @Query(
+        """
+        SELECT *
+        FROM hospitalentity
+        WHERE LOWER(hospitalName) LIKE '%' || LOWER(:query) || '%' OR UPPER(:query) == hospitalName
+    """
+    )
+    suspend fun searchHospitalEntity(query: String): List<HospitalEntity>
+}
